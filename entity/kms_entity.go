d9627b95cfeb299c44584951ac2318c528c6d949
caoxudong <caoxudong818@gmail.com>
2026-06-30 18:52:25 +0800

    add entities according to sql

---
+++ entity/kms_entity.go
@@ -0,0 +1,28 @@
+package entity
+
+import "time"
+
+type Project struct {
+	Id          int64     `json:"id" xorm:"id,pk,autoincr"`
+	ProjectName string    `json:"project_name" xorm:"project_name,varchar"`
+	CreateTime  time.Time `json:"create_time" xorm:"create_time"`
+	UpdateTime  time.Time `json:"update_time" xorm:"update_time"`
+}
+
+type ProjectToken struct {
+	Id                     int64     `json:"id" xorm:"id,pk,autoincr"`
+	ProjectId              int64     `json:"project_id" xorm:"project_id"`
+	ProjectToken           string    `json:"project_token" xorm:"project_token"`
+	ProjectTokenExpireTime time.Time `json:"project_token_expire_time" xorm:"project_token_expire_time"`
+	CreateTime             time.Time `json:"create_time" xorm:"create_time"`
+	UpdateTime             time.Time `json:"update_time" xorm:"update_time"`
+}
+
+type ProjectKey struct {
+	Id                int64     `json:"id" xorm:"id,pk,autoincr"`
+	ProjectId         int64     `json:"project_id" xorm:"project_id"`
+	ProjectKey        string    `json:"project_key" xorm:"project_key"`
+	ProjectKeyContent string    `json:"project_key_content" xorm:"project_key_content"`
+	CreateTime        time.Time `json:"create_time" xorm:"create_time"`
+	UpdateTime        time.Time `json:"update_time" xorm:"update_time"`
+}
--- sql/databse.sql
+++ sql/databse.sql
@@ -3,7 +3,6 @@ Create Database keys;
 Create Table projects (
     id bigint primary key auto_increment,
     project_name varchar(128) not null defualt '',
-    register_time datetime default current_timestamp,
     create_time datetime default current_timestamp,
     update_time datetime default current_timestamp on update current_timestamp,
     uk_project_name unique (project_name)
@@ -25,7 +24,7 @@ Create table keys (
     id bigint primary key auto_increment,
     project_id bigint not null default 0,
     project_key varchar(128) not null default '',
-    project_content text not null default '',
+    project_key_content text not null default '',
     create_time datetime default current_timestamp,
     update_time datetime default current_timestamp on update current_timestamp,
     uk_project_key unique (project_id, project_key),
